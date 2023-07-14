63fd467a7ccdff8f95a8e1b242a287f665758367
kennataddese6 <kennataddese6@gmail.com>
2023-07-14 13:47:30 +0300

    add icon to attach image button

--- AttachButton.js
+++ AttachButton.js
@@ -1,11 +1,19 @@
 import React, {useState} from 'react';
-import {Button, Image, StyleSheet, TouchableOpacity, View, Text} from 'react-native';
+import {
+  Button,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+} from 'react-native';
 import * as ImagePicker from 'react-native-image-picker';
-
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 export default function AttachButton() {
   const [image, setImage] = useState(null);
 
   const pickImage = () => {
+    console.log('here are the icons if any', MaterialIcons);
     ImagePicker.launchImageLibrary({}, response => {
       if (response.didCancel) {
         console.log('User cancelled image picker');
@@ -21,31 +29,27 @@ export default function AttachButton() {
 
   return (
     <View style={styles.container}>
-    <TouchableOpacity style={styles.button} onPress={pickImage}>
-      <Text style={styles.buttonText}>Select Image</Text>
-    </TouchableOpacity>
-    {image && <Image source={{ uri: image }} style={styles.image} />}
-  </View>
+      <TouchableOpacity style={styles.button} onPress={pickImage}>
+        <MaterialIcons name="attach-file" size={24} color="black" />
+      </TouchableOpacity>
+      {image && <Image source={{uri: image}} style={styles.image} />}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    button: {
-      backgroundColor: 'grey',
-      borderRadius: 5,
-    },
-    buttonText: {
-      color: 'white',
-      fontSize: 16,
-    },
-    image: {
-      marginTop: 20,
-      width: 200,
-      height: 200,
-    },
-  });
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    borderRadius: 5,
+    color:'green'
+  },
+  image: {
+    marginTop: 20,
+    width: 200,
+    height: 200,
+  },
+});
