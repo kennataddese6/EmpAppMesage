38cb08bcd26a7ad5ee8ff1b2b639a5536c0e87b6
kennataddese6 <kennataddese6@gmail.com>
2023-07-26 14:28:19 +0300

    record audio and convert in to base64 data

--- RecordAudio.js
+++ RecordAudio.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import {
-  StyleSheet,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, TouchableOpacity, View} from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import { AudioRecorder, AudioUtils } from 'react-native-audio';
-
-export default function AttachButton({ information }) {
+import {AudioRecorder, AudioUtils} from 'react-native-audio';
+import RNFS from 'react-native-fs';
+import {encode} from 'base64-js';
+export default function AttachButton({information}) {
   const [isRecording, setIsRecording] = useState(false);
-  const [audioPath, setAudioPath] = useState(AudioUtils.DocumentDirectoryPath + '/test.aac');
+  const [audioPath, setAudioPath] = useState(
+    AudioUtils.DocumentDirectoryPath + '/test.aac',
+  );
 
   const startRecording = async () => {
+    console.log('start recording');
     try {
       await AudioRecorder.prepareRecordingAtPath(audioPath, {
         SampleRate: 22050,
@@ -28,10 +28,29 @@ export default function AttachButton({ information }) {
   };
 
   const stopRecording = async () => {
+    console.log('stop recording');
     try {
       const filePath = await AudioRecorder.stopRecording();
       setIsRecording(false);
       setAudioPath(filePath);
+      const audioData = await RNFS.readFile(filePath, 'base64');
+      const blob = new Blob([audioData], {type: 'audio/aac'});
+      console.log('this is the size of the blob',blob.size)
+      if (blob.size <= 11800) {
+        const base64String = await new Promise((resolve, reject) => {
+          const reader = new FileReader();
+          reader.onerror = reject;
+          reader.onload = () => {
+            resolve(reader.result);
+          };
+          reader.readAsDataURL(blob);
+        });
+
+        // Store the base64-encoded audio data in a state variable
+        console.log('this is the base64data', base64String.length);
+      } else {
+        console.warn('Audio file is too large to be uploaded');
+      }
     } catch (error) {
       console.error(error);
     }
